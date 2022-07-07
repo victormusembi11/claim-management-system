afa5520e7e9a61467a7df70ccc242cd79c22a76a
victormusembi11 <victor.musembi11@gmail.com>
2022-07-07 13:43:04 -0400

    style: line was > 79 chars.

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('about', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('about', TemplateView.as_view(
+            template_name='about.html'
+        ), name='about'),
 ]
