a43eb2b7595e1c4fbd8019613723a84247590df2
victormusembi11 <victor.musembi11@gmail.com>
2022-07-20 18:32:55 +0300

    feat: New allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = get_bool_env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
