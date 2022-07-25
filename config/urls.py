cbcd220a9bef26673be0198cacfb846ee72f533d
victormusembi11 <victor.musembi11@gmail.com>
2022-07-25 09:14:48 +0300

    feat: path to core app urls

--- config/urls.py
+++ config/urls.py
@@ -16,13 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('about', TemplateView.as_view(
-            template_name='about.html'
-        ), name='about'),
+    path('', include('core.urls'), name='core'),
     path('accounts/', include('accounts.urls'), name='accounts'),
 ]
