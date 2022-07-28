0fd9e34f44ec9a561ffb8b314d61c31a94eab0bf
victormusembi11 <victor.musembi11@gmail.com>
2022-07-28 17:33:07 +0300

    fix: test setup had no return value

--- tests/core/test_views.py
+++ tests/core/test_views.py
@@ -7,6 +7,7 @@ class TestCoreAppView(TestCase):
     def setUp(self):
         self.home_url = reverse('core:home')
         self.about_url = reverse('core:about')
+        return super().setUp()
 
     def test_home_view(self):
         response = self.client.get(self.home_url)
