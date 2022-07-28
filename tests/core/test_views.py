15934c9db57b437f7bdf02756955318be6883523
victormusembi11 <victor.musembi11@gmail.com>
2022-07-28 17:41:40 +0300

    refactor: improved the test methods name

--- tests/core/test_views.py
+++ tests/core/test_views.py
@@ -9,10 +9,10 @@ class TestCoreAppView(TestCase):
         self.about_url = reverse('core:about')
         return super().setUp()
 
-    def test_home_view(self):
+    def test_home_view_success(self):
         response = self.client.get(self.home_url)
         self.assertEqual(response.status_code, 200)
 
-    def test_about_view(self):
+    def test_about_view_success(self):
         response = self.client.get(self.about_url)
         self.assertEqual(response.status_code, 200)
