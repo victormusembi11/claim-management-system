2b50e445729cfe1db6f0a9256a2e0d522d431eb9
victormusembi11 <victor.musembi11@gmail.com>
2022-08-02 21:42:45 +0300

    feat: bootstrap form widgets for registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,3 +9,12 @@ class UserRegistrationForm(UserCreationForm):
         fields = (
             'username', 'email'
         )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update(
+                {
+                    'class': 'form-control'
+                }
+            )
