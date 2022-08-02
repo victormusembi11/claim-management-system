9f626b1b6fab0129976d4ad1c6246ee1842bac5a
victormusembi11 <victor.musembi11@gmail.com>
2022-08-02 15:30:04 +0300

    feat: user register added to path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import authentication as auth
+from .views import registration as reg
 
 app_name = 'accounts'
 urlpatterns = [
     path('login/', auth.UserLoginView.as_view(), name='login'),
     path('logout/', auth.UserLogoutView.as_view(), name='logout'),
+    path('register/', reg.RegistrationView.as_view(), name="register")
 ]
