ea2be45b199bf0f513b778ca3a2a125d5e3a068e
victormusembi11 <victor.musembi11@gmail.com>
2022-08-03 10:53:20 +0300

    test: changed to pytest from TestCase

--- tests/core/test_views.py
+++ tests/core/test_views.py
@@ -1,18 +1,20 @@
-from django.test import TestCase, Client
+import pytest
+from django.test import Client
 from django.urls import reverse_lazy
 
 
-class TestCoreViews(TestCase):
+@pytest.fixture
+def client():
+    yield Client()
 
-    def setUp(self):
-        self.home_url = reverse_lazy('core:home')
-        self.about_url = reverse_lazy('core:about')
-        self.client = Client()
 
-    def test_home_view(self):
-        response = self.client.get(self.home_url)
-        self.assertEqual(response.status_code, 200)
+def test_home_view(client):
+    home_url = reverse_lazy('core:home')
+    response = client.get(home_url)
+    assert response.status_code == 200
 
-    def test_about_view(self):
-        response = self.client.get(self.about_url)
-        self.assertEqual(response.status_code, 200)
+
+def test_about_view(client):
+    about_url = reverse_lazy('core:about')
+    response = client.get(about_url)
+    assert response.status_code == 200
