9c1fdc0518ba90a65da1874e7297b73acca8a7a7
victormusembi11 <victor.musembi11@gmail.com>
2022-08-03 10:23:35 +0300

    fix: resolve to correct login path after register success

--- accounts/views/registration.py
+++ accounts/views/registration.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from ..forms import UserRegistrationForm
@@ -7,4 +8,4 @@ class RegistrationView(CreateView):
 
     form_class = UserRegistrationForm
     template_name = 'registration/register.html'
-    success_url = 'registration/login'
+    success_url = reverse_lazy('accounts:login')
